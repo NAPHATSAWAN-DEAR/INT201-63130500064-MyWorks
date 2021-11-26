2be665dc7d6518dd82c81200988a7d0759b7d3a2
unknown <naphatsawan.bw@gmail.com>
2021-11-26 22:07:12 +0700

    assignment

---
+++ assignmentinclasss/addDom.js
@@ -0,0 +1,27 @@
+/*
+const group7 = document.querySelector("#Section2 .group7");
+document.querySelector("#Section2 .group7").style.color="red";
+group7.innerHTML = "deeja";
+*/
+
+//กลุ่ม 7/*
+var attr = document.createAttribute("style");
+attr.value = "color: red";
+const group7 = document.querySelector("#Section2 .group7");
+group7.innerHTML = "Love Aj.Umaporn";
+group7.setAttributeNode(attr);
+
+
+
+/*
+const group1 = document.querySelector("#Section2 .group1");
+group1.innerHTML = "work hard play harder";
+group1.style.color = 'Blue';
+group1.style.fontSize = "larger";*/
+
+//const g7 = document.getElementById("#Section2 .group7").style.color="red";
+//const group7 = document.getElementById("#Section2 .group7").style.color = "red";
+//const g7 = document.getElementId("#Section2 .group7").style.color = 'red';
+//g7.innerHTML = "deeja";
+//group7.style.color = 'red';
+//g7;
